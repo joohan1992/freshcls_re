f63297f9f9eee9da58c09df65d3edad4f8aabf09
jhseong <jhseong@businessinsight.co.kr>
2022-11-29 14:52:40 +0900

    중간 커밋(피드백 전송 기능 개발전)

--- lib/main.dart
+++ lib/main.dart
@@ -36,45 +36,45 @@ class NoCheckCertificateHttpOverrides extends io.HttpOverrides {
 }
 
 requestInference(camlib.XFile xfile) async {
-  // var file = File(xfile.path);
-
   String url = 'https://192.168.0.88:5443/run';
-  // print(bytes);
+  var postUri = Uri.parse(url);
+
+  String CRUDENTIAL_KEY = "7{@:M8IR;DW\\/X71uhHOd[nxa@uB%+m(/<Owq5LZ.kO%K583{t-fDb'GkE\$YscX?N`X}M=WnMC<Ed}a4.\$.lvDPL=q;i237fvcDjPPXmY`r.FU`@D*nQ]mBTNb#t7_Qw*Tr?f6]aTWm},Z(8L&^xI\$^5Ccru'a.}'/uaN+{d\\Ox#FWv(ZT,>8vVC}kc2q2&'.qddiHnN}^*L]A*ZMT,{soMw@BrppFG[OIrv_bD/b67H:H0-;dxDID/Y[Yhz{y~VUVG|(aZ]]xj[jB*q)ARPA>)S._*JH]iE!zlnFzBatlkAfvy";
 
   img.Image? image = img.decodeImage(await xfile.readAsBytes());
 
-  // Resize the image to a 120x? thumbnail (maintaining the aspect ratio).
-  img.Image thumbnail = img.copyResize(image!, width: 299, height: 299);
+  final face = img.copyCrop(
+    image!,
+    0,
+    (image.height-image.width) ~/ 2,
+    image.width,
+    image.width,
+  );
+  img.Image thumbnail = img.copyResize(face, width: 299, height: 299);
 
-  print(thumbnail.length);
-  print(img.encodeJpg(thumbnail).length);
-  print(thumbnail.getPixel(0, 0));
+  var byteImg = thumbnail.getBytes();
+  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  String encoded = stringToBase64.encode(byteImg.toString());
+  var header = {"Content-type": "application/json", 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST, GET'};
+  http.Response res = await http.post(postUri, headers:header, body: json.encode({
+    "image": encoded,
+    "x_size": '299',
+    "y_size": '299',
+    "channel": '3',
+    "key": CRUDENTIAL_KEY,
+    "auth": 'code',
+    "ID": 'None',
+    "PW": 'None',
+  }));
 
-  var postUri = Uri.parse(url);
-  var request = http.MultipartRequest("POST", postUri);
-  Map<String, String> headers = {"Content-type": "multipart/form-data"};
-  request.fields['test'] = '123';
-  request.files.add(http.MultipartFile.fromBytes('file', img.encodeJpg(thumbnail), filename: 'attachFile', contentType: MediaType('image', 'jpeg')),);
-  //request.files.add(http.MultipartFile('file',file.readAsBytes().asStream(), file.lengthSync(),filename: 'attachFile',),);
-  // request.files.add(http.MultipartFile.fromBytes('file', await file.readAsBytes(), contentType: MediaType('image', 'jpeg')));
-  request.headers.addAll(headers);
+  String resBody = utf8.decode(res.bodyBytes);
+  Map<String, dynamic> resultMap = jsonDecode(resBody);
 
-  var res = await request.send();
+  resultMap['thumbnail'] = img.encodeJpg(thumbnail);
+  print(byteImg);
+  print(byteImg.length);
 
-  res.stream.transform(utf8.decoder).listen((value) {
-    print(value);
-  });
-
-  return res;
-}
-
-requestPost() async {
-  String url = 'https://192.168.0.88/test';
-  var postUri = Uri.parse(url);
-  var res = await http.post(postUri, body: {
-    'val1': 'test'
-  });
-  print(res.body);
+  return resultMap;
 }
 
 class CameraApp extends StatefulWidget {
@@ -87,7 +87,7 @@ class _CameraAppState extends State<CameraApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text('Camera test')),
+          appBar: AppBar(title: Text('R-pha Vision V1.0')),
           body: AppBody(),
         ));
   }
@@ -144,7 +144,6 @@ class _AppBodyState extends State<AppBody> {
   }
 }
 
-
 class CameraView extends StatefulWidget {
   final List<camlib.CameraDescription> cameras;
 
@@ -171,7 +170,13 @@ class _CameraViewState extends State<CameraView> with TickerProviderStateMixin{
   bool flashLight = false;
   bool orientationLocked = false;
 
+  Uint8List? captured = null;
+  bool suspending = false;
   bool isLoading = false;
+  camlib.CameraPreview? cameraPreview = null;
+  Widget? previewFrame = null;
+
+  List<Widget> list_results = [];
 
   late TabController _tabController;
 
@@ -179,14 +184,11 @@ class _CameraViewState extends State<CameraView> with TickerProviderStateMixin{
 
     setState(() {
       controller = camlib.CameraController(description, camlib.ResolutionPreset.max);
+      cameraPreview = camlib.CameraPreview(controller!);
     });
 
     try {
-      print("!?");
-      print(description);
-      print(controller);
       await controller!.initialize();
-      print("?!");
 
       final minZoom = await controller!.getMinZoomLevel();
       final maxZoom = await controller!.getMaxZoomLevel();
@@ -233,7 +235,8 @@ class _CameraViewState extends State<CameraView> with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    final width = MediaQuery.of(context).size.width;
+
     if (error != null) {
       return Center(
         child: Text('Initializing error: $error\nCamera list:'),
@@ -273,316 +276,178 @@ class _CameraViewState extends State<CameraView> with TickerProviderStateMixin{
       controller: _tabController,
     );
 
-    List<Widget> list_results = [];
+    print(controller!.value.aspectRatio);
 
-    camlib.CameraPreview cameraPreview = camlib.CameraPreview(controller!);
-
-    Widget viewToShow = cameraPreview;
+    Widget? viewToShow = null;
     if (isLoading) {
       viewToShow = Image(image: AssetImage('assets/loading.jpg'));
+    } else if (suspending) {
+      viewToShow = Image.memory(captured!);
+    } else {
+      viewToShow = ClipRect(
+        child: OverflowBox(
+          alignment: Alignment.center,
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Container(
+              width: width,
+              height:
+              width * controller!.value.aspectRatio,
+              child: cameraPreview, // this is my CameraPreview
+            ),
+          ),
+        ),
+      );
+    }
+
+    sendFeedback(context, infer_no, title, list_label) {
+      print(title);
+
+      if (title == null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                LabelSeletionScreen(infer_no: infer_no, predict_labels: list_label,),
+          ),
+        );
+      }
     }
 
     SingleChildScrollView cameraView = SingleChildScrollView(
-      child: Column(children: [
-        AspectRatio(aspectRatio: 1.4/1,
+      child: Column(
+        children: [
+        Container(
+          width: width,
+          height: (suspending && !isLoading) ? width*0.5 : width,
           child: viewToShow,
         ),
         Container(
-          child: Text("Results"),
+          height: (suspending && !isLoading) ? 20 : 0,
+          child: const Text("Results"),
         ),
         Container(
-          height: 200,
+          height: (suspending && !isLoading) ? width*0.5 + 50 : 70,
           child:
-            SingleChildScrollView(
-              child: Column(
-                children: list_results,
-              ),
+          SingleChildScrollView(
+            child: Column(
+              children: list_results,
             ),
+          ),
         ),
-        // if (!recording)
-        //   ElevatedButton(
-        //     onPressed: controller == null
-        //         ? null
-        //         : () async {
-        //       await controller!.startVideoRecording();
-        //       setState(() {
-        //         recording = true;
-        //       });
-        //     },
-        //     child: Text('Record video'),
-        //   ),
-        // if (recording)
-        //   ElevatedButton(
-        //     onPressed: () async {
-        //       final file = await controller!.stopVideoRecording();
-        //       final bytes = await file.readAsBytes();
-        //       final uri =
-        //       Uri.dataFromBytes(bytes, mimeType: 'video/webm;codecs=vp8');
-        //
-        //       final link = html.AnchorElement(href: uri.toString());
-        //       link.download = 'recording.webm';
-        //       link.click();
-        //       link.remove();
-        //       setState(() {
-        //         recording = false;
-        //       });
-        //     },
-        //     child: Text('Stop recording'),
-        //   ),
-        // SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: controller == null
-              ? null
-              : () async {
+        Container(
+          height: 30,
+          child: ElevatedButton(
+            onPressed: (controller == null || isLoading) ? null : () async {
+              if (suspending) {
+                setState(() {
+                  suspending = false;
+                  list_results = [];
+                });
+              } else {
+                setState(() {
+                  isLoading = true;
+                  suspending = true;
+                });
+                controller!.lockCaptureOrientation();
 
-            setState(() {
-              isLoading = true;
-            });
-            controller!.lockCaptureOrientation();
+                camlib.XFile xfile = await controller!.takePicture();
+                Uint8List byteImg = await xfile.readAsBytes();
 
-            camlib.XFile xfile = await controller!.takePicture();
-            Uint8List byteImg = await xfile.readAsBytes();
-
-            print(byteImg);
-            var response = await requestInference(xfile);
-            setState(() {
-              isLoading = false;
-            });
-            print(response.stream.bytesToString());
-
-            // 사진을 촬영하면, 새로운 화면으로 넘어갑니다.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(image: byteImg),
-              ),
-            );
-          },
-          child: Text('Take picture'),
+                Map<String, dynamic> resultMap = await requestInference(xfile);
+                captured = resultMap['thumbnail'];
+                int infer_no = resultMap['infer_no'];
+                List<Widget> tmp_list_results = [];
+                List<dynamic> list_infer = resultMap['cls_list'];
+                for (var element in list_infer) {
+                  tmp_list_results.add(
+                      ElevatedButton(
+                        onPressed: () async {sendFeedback(context, infer_no, element, list_infer); },
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: Size(width, 30) // put the width and height you want
+                        ),
+                        child: Text(element),
+                      )
+                  );
+                }
+                tmp_list_results.add(
+                    ElevatedButton(
+                      onPressed: () async {sendFeedback(context, infer_no, null, list_infer); },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: Size(width, 30) // put the width and height you want
+                      ),
+                      child: const Text('기타'),
+                    )
+                );
+                tmp_list_results.add(
+                    ElevatedButton(
+                      onPressed: () async {sendFeedback(context, infer_no, null, list_infer); },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: Size(width, 30) // put the width and height you want
+                      ),
+                      child: const Text('기타'),
+                    )
+                );
+                tmp_list_results.add(
+                    ElevatedButton(
+                      onPressed: () async {sendFeedback(context, infer_no, null, list_infer); },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: Size(width, 30) // put the width and height you want
+                      ),
+                      child: const Text('기타'),
+                    )
+                );
+                setState(() {
+                  list_results = tmp_list_results;
+                  isLoading = false;
+                });
+              }
+            },
+            child: Text(isLoading ? 'Waiting' : (suspending ? 'Retry' : 'Take picture')),
+          ),
         ),
-        // SizedBox(height: 10),
-        // if (!orientationLocked)
-        //   ElevatedButton(
-        //       onPressed: () {
-        //         controller!.lockCaptureOrientation();
-        //         setState(() {
-        //           orientationLocked = true;
-        //         });
-        //       },
-        //       child: Text('Lock orientation')),
-        // if (orientationLocked)
-        //   ElevatedButton(
-        //       onPressed: () {
-        //         controller!.unlockCaptureOrientation();
-        //         setState(() {
-        //           orientationLocked = false;
-        //         });
-        //       },
-        //       child: Text('Unlock orientation')),
-        // SizedBox(height: 10),
-        // if (!flashLight)
-        //   ElevatedButton(
-        //       onPressed: () {
-        //         controller!.setFlashMode(camlib.FlashMode.always);
-        //         setState(() {
-        //           flashLight = true;
-        //         });
-        //       },
-        //       child: Text('Turn flashlight on')),
-        // if (flashLight)
-        //   ElevatedButton(
-        //       onPressed: () {
-        //         controller!.setFlashMode(camlib.FlashMode.off);
-        //         setState(() {
-        //           flashLight = false;
-        //         });
-        //       },
-        //       child: Text('Turn flashlight off')),
-        // SizedBox(height: 10),
-        // if (zoomLevel != null && maxZoom != null)
-        //   Text('Zoom level: $zoomLevel/$maxZoom'),
-        // if (zoomLevel != null && minZoom != null && maxZoom != null)
-        //   Slider(
-        //     value: zoomLevel!,
-        //     onChanged: (newValue) {
-        //       setState(() {
-        //         zoomLevel = newValue;
-        //       });
-        //       controller!.setZoomLevel(newValue);
-        //     },
-        //     min: minZoom!,
-        //     max: maxZoom!,
-        //   ),
-        // if (exposure != null && maxExposure != null)
-        //   Text('Exposure offset: $exposure/$maxExposure'),
-        // if (exposure != null && minExposure != null && maxExposure != null)
-        //   Slider(
-        //     value: exposure!,
-        //     onChanged: (newValue) {
-        //       setState(() {
-        //         exposure = newValue;
-        //       });
-        //       controller!.setExposureOffset(newValue);
-        //     },
-        //     min: minExposure!,
-        //     max: maxExposure!,
-        //   ),
-        // SizedBox(height: 10),
       ],
       ),
     );
 
     return Column(children: [
       tabBar,
-        Expanded(
-          child: TabBarView(
-            controller: _tabController,
-            children: [
-              cameraView,
-              Container(
-                color: Colors.green[200],
-                alignment: Alignment.center,
-                child: Text(
-                  'Tab2 View',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
+      Expanded(
+        child: TabBarView(
+          controller: _tabController,
+          children: [
+            cameraView,
+            Container(
+              color: Colors.green[200],
+              alignment: Alignment.center,
+              child: const Text(
+                'Tab2 View',
+                style: TextStyle(
+                  fontSize: 30,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    );
+      ),
+    ],);
   }
 }
 
+// 기타 선택
+class LabelSeletionScreen extends StatelessWidget {
+  final int infer_no;
+  final List<dynamic> predict_labels;
 
-class _MediaSizeClipper extends CustomClipper<Rect> {
-  final Size mediaSize;
-  const _MediaSizeClipper(this.mediaSize);
-  @override
-  Rect getClip(Size size) {
-    return Rect.fromLTWH(0, 0, mediaSize.width, mediaSize.height);
-  }
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return true;
-  }
-}
-
-// A screen that takes in a list of cameras and the Directory to store images.
-class TakePictureScreen extends StatefulWidget {
-  final camlib.CameraDescription camera;
-
-  const TakePictureScreen({
-    Key? key,
-    required this.camera,
-  }) : super(key: key);
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  // CameraController와 Future를 저장하기 위해 두 개의 변수를 state 클래스에
-  // 정의합니다.
-  late camlib.CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 카메라의 현재 출력물을 보여주기 위해
-    // CameraController를 생성합니다.
-    _controller = camlib.CameraController(
-      // Get a specific camera from the list of available cameras.
-      // 이용 가능한 카메라 목록에서 특정 카메라를 가져옵니다.
-      widget.camera,
-      // 적용할 해상도를 지정합니다.
-      camlib.ResolutionPreset.medium,
-    );
-
-    // 다음으로 controller를 초기화합니다. 초기화 메서드는 Future를 반환합니다.
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // 위젯의 생명주기 종료시 컨트롤러 역시 해제시켜줍니다.
-    _controller.dispose();
-    super.dispose();
-  }
+  const LabelSeletionScreen({Key? key, required this.infer_no, required this.predict_labels}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      // 카메라 프리뷰를 보여주기 전에 컨트롤러 초기화를 기다려야 합니다. 컨트롤러 초기화가
-      // 완료될 때까지 FutureBuilder를 사용하여 로딩 스피너를 보여주세요.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // Future가 완료되면, 프리뷰를 보여줍니다.
-            return camlib.CameraPreview(_controller);
-          } else {
-            // 그렇지 않다면, 진행 표시기를 보여줍니다.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        // onPressed 콜백을 제공합니다.
-        onPressed: () async {
-          // try / catch 블럭에서 사진을 촬영합니다. 만약 뭔가 잘못된다면 에러에
-          // 대응할 수 있습니다.
-          try {
-            // 카메라 초기화가 완료됐는지 확인합니다.
-            await _initializeControllerFuture;
-
-            // 사진 촬영을 시도하고 저장되는 경로를 로그로 남깁니다.
-            camlib.XFile xfile = await _controller.takePicture();
-            Uint8List byteImg = await xfile.readAsBytes();
-
-            //print(bytes);
-
-            var response = await requestInference(xfile);
-            print(response.stream.bytesToString());
-
-            // 사진을 촬영하면, 새로운 화면으로 넘어갑니다.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(image: byteImg),
-              ),
-            );
-          } catch (e) {
-            // 만약 에러가 발생하면, 콘솔에 에러 로그를 남깁니다.
-            print(e);
-          }
-        },
-      ),
-    );
-  }
-}
-
-
-// 사용자가 촬영한 사진을 보여주는 위젯
-class DisplayPictureScreen extends StatelessWidget {
-  final Uint8List image;
-
-  const DisplayPictureScreen({Key? key, required this.image}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
+      appBar: AppBar(title: const Text('Choose a Label')),
       // 이미지는 디바이스에 파일로 저장됩니다. 이미지를 보여주기 위해 주어진
       // 경로로 `Image.file`을 생성하세요.
-      body: Image.memory(image),
+      body: SingleChildScrollView(child: Column(children: [],)),
     );
   }
 }
